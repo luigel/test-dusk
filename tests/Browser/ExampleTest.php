79b196990e0461e34bd7f54a4e6bf7cdf4cd4468
Rigel Kent Carbonel <rigel20.kent@gmail.com>
2022-02-23 22:55:22 +0800

    test

--- tests/Browser/ExampleTest.php
+++ tests/Browser/ExampleTest.php
@@ -13,7 +13,6 @@ test('example test', function () {
 it('can login', function () {
    $this->browse(function (Browser $browser) {
        $user = \App\Models\User::factory()->create();
-    dd(\App\Models\User::all());
 
        $browser->visit(new LoginPage())
            ->waitFor('@email')
